6f17579c401b81cabbc4859f6111f65e57aef3ce
PercTruman <drshawnhart@gmail.com>
2022-09-14 16:37:59 -0500

    drop column from subjects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_182857) do
+ActiveRecord::Schema.define(version: 2022_09_14_213631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2022_09_14_182857) do
     t.string "name"
     t.string "room_number"
     t.time "time"
-    t.integer "subject_id"
     t.integer "teacher_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
