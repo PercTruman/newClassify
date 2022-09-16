10fc6bd103b81011500c188231ec4515c5f21697
PercTruman <drshawnhart@gmail.com>
2022-09-16 15:47:51 -0500

    added new checkbox window

--- client/src/AddStudentsDialog.js
+++ client/src/AddStudentsDialog.js
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "./context/UserContext";
 
 import Button from "@mui/material/Button";
 // import TextField from "@mui/material/TextField";
-import Checkbox from "@mui/material/Checkbox";
 
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -11,9 +10,13 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-function AddStudentsDialog({subjectId}) {
+function AddStudentsDialog({ subjectId }) {
   const { students } = useContext(UserContext);
   const [open, setOpen] = React.useState(false);
+  //   const [classArray, setClassArray] = useState([]);
+  const [checkedState, setCheckedState] = useState(
+    new Array(students.length).fill(false)
+  );
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -23,36 +26,47 @@ function AddStudentsDialog({subjectId}) {
     setOpen(false);
   };
 
-  const studentCheckboxes = students.map((s) => (
-    <div>
-        <label>{s.name}</label>
-      <Checkbox value={s.id}/>
-      {/* {s.name} */}
-    </div>
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
 
-    
-  ))
-  
+    setCheckedState(updatedCheckedState);
+  };
+
+  const studentCheckboxes = students.map((s, index) => (
+    <div key={s.id}>
+      <input
+        type="checkbox"
+        id={`custom-checkbox-${index}`}
+        name={s.name}
+        value={s.name}
+        checked={checkedState[index]}
+        onChange={() => handleOnChange(index)}
+      />
+      <label htmlFor={`custom-checkbox-${index}`}>{s.name}</label>
+    </div>
+  ));
+
   const submitForUpdate = (e, subjectId) => {
     e.preventDefault();
-    fetch('/student_subjects', {
+
+    fetch("/student_subjects", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-    //    student_id:,
-       subject_id: subjectId
+        //    student_id:,
+        subject_id: subjectId,
       }),
-    })
-      .then((res) => res.json())
+    }).then((res) => res.json());
     //   .then((updatedSubject) => {
     //     const updatedSubjectList = subjects.map((s) =>
     //       s.id === Number(updatedSubject.id) ? updatedSubject : s
     //     );
     //     setSubjects(updatedSubjectList);
     //   });;
-  }
-    return (
-
+  };
+  return (
     <div>
       <Button variant="contained" onClick={handleClickOpen}>
         Add Students
@@ -62,30 +76,20 @@ function AddStudentsDialog({subjectId}) {
           <DialogTitle>Add Students to Class</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              To add a student to this class, click the checkbox next to their name.
+              To add a student to this class, click the checkbox next to their
+              name.
             </DialogContentText>
 
             {studentCheckboxes}
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
-            <Button type="submit" >
-              Save Changes
-            </Button>
-            <Button
-              onClick={() => {
-                // handleClassDelete();
-                handleClose();
-              }}
-            >
-              Delete Class
-            </Button>
+            <Button type="submit">Add students</Button>
           </DialogActions>
         </form>
       </Dialog>
     </div>
   );
 }
-
 
 export default AddStudentsDialog;
