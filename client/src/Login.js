e25a5b42d0b6d5aa9d69cb9601ffed12fe455ab1
PercTruman <drshawnhart@gmail.com>
2022-09-27 20:26:06 -0500

    complete LogIn and fix buttons

--- client/src/Login.js
+++ client/src/Login.js
@@ -47,39 +47,55 @@ const Login = () => {
     <div>
       <Grid sx={{ justifyContent: "center" }} container spacing={2}>
         <Grid item>
-      <form onSubmit={handleSubmit}>
-        <h2 style = {{marginLeft:"4rem"}}>Login</h2>
-        <TextField 
-        sx={{marginBottom: "2rem"}}
-          size = "small"
-          label="Username"
-          name="username"
-          type="text"
-          autoComplete="on"
-          id="username"
-          value={formData.username}
-          onChange={handleChange}
-        />{" "}
-        <br />
-        
-        <TextField 
-          label = "Password"
-          size = "small"
-          name="password"
-          type="password"
-          autoComplete="on"
-          id="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <br />
-        <Button sx={{ marginLeft: "4rem", marginTop: "2rem" }} variant="contained" onClick={() => navigate("/")}>
-           Back
-          </Button>
-      </form>
-      </Grid>
-    
-      <ul>{errorsList}</ul>
+          <form onSubmit={handleSubmit}>
+            <h2 style={{ marginLeft: "4rem" }}>Login</h2>
+            <TextField
+              sx={{ marginBottom: "2rem" }}
+              size="small"
+              label="Username"
+              name="username"
+              type="text"
+              autoComplete="on"
+              id="username"
+              value={formData.username}
+              onChange={handleChange}
+            />{" "}
+            <br />
+            <TextField
+              label="Password"
+              size="small"
+              name="password"
+              type="password"
+              autoComplete="on"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <br />
+            <Grid sx={{ justifyContent: "center" }} container spacing={2}>
+              <Grid item>
+                <Button
+                  sx={{ marginLeft: "1.5rem", marginTop: "2rem" }}
+                  variant="contained"
+                  onClick={() => navigate("/")}
+                >
+                  Back
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  sx={{ marginRight: "2rem", marginTop: "2rem" }}
+                  variant="contained"
+                  onClick={() => navigate("/signup")}
+                >
+                  Signup
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+
+        <ul>{errorsList}</ul>
       </Grid>
     </div>
   );
