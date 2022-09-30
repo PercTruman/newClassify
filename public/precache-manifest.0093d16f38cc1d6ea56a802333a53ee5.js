1fd68eee4afd2ec764cea504075d6bc867e10518
PercTruman <drshawnhart@gmail.com>
2022-09-30 08:47:00 -0500

    add dependency

---
+++ public/precache-manifest.0093d16f38cc1d6ea56a802333a53ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1936143136caf78d298",
+    "url": "/static/css/main.0ad79521.chunk.css"
+  },
+  {
+    "revision": "a1936143136caf78d298",
+    "url": "/static/js/main.1c0b4546.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65c1ee5669dd7d198563",
+    "url": "/static/js/2.3712a524.chunk.js"
+  },
+  {
+    "revision": "edb642cf2f19c533b0db447cd02a5967",
+    "url": "/index.html"
+  }
+];
